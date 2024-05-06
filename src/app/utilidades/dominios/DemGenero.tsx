bd1c3394ee57469ab0c869b0465e7fc7d40c52bb
arolong <arolong4@cuc.edu.co>
2024-05-06 12:31:34 -0500

    m1

---
+++ src/app/utilidades/dominios/DemGenero.tsx
@@ -0,0 +1,9 @@
+import { PeliculaGenero } from "../../modelos/PeliculaGenero";
+
+export const ARREGLO_PELICULA_GENERO: Array<PeliculaGenero>=[
+    {codGenero: "1", nombreGenero: "Accion"},
+    {codGenero: "2", nombreGenero: "Drama"},
+    {codGenero: "3", nombreGenero: "Ciencia-Ficción"},
+    {codGenero: "4", nombreGenero: "Suspenso"},
+    {codGenero: "5", nombreGenero: "Terror"},
+];
